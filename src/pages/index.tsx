72158d3483e4d16ba58bb1bf809b120a91a66f2f
minumsa <icycyi@naver.com>
2023-04-11 23:49:01 +0900

    posts.tsx 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,50 @@
 import * as React from "react";
-import { PageProps, graphql } from "gatsby";
+import { PageProps, graphql, Link } from "gatsby";
 import LandingPage from "../components/views/LandingPage/LandingPage";
 import Layout from "../components/Layout";
-import Link from "gatsby-link";
 
-const Index: React.FC = () => {
-  return (
-    <Layout>
-      <LandingPage />
-    </Layout>
-  );
-};
-// const IndexQuery = ({ data }: any) => {
+// const Index: React.FC = () => {
 //   return (
-//     <div>
-//       <h2>Posts</h2>
-//       <ul>
-//         {data.allMarkdownRemark.edges.map(({ node }: any) => (
-//           <li key={node.id}>
-//             <Link to={node.frontmatter.slug}>{node.frontmatter.title}</Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
+//     <Layout>
+//       <LandingPage />
+//     </Layout>
 //   );
 // };
 
-// export default IndexQuery;
+// export default Index;
 
-// export const query = graphql`
-//   query IndexQuery {
-//     allMarkdownRemark {
-//       totalCount
-//       edges {
-//         node {
-//           id
-//           frontmatter {
-//             title
-//             date(formatString: "DD MMMM, YYYY")
-//             slug
-//           }
-//           excerpt
-//         }
-//       }
-//     }
-//   }
-// `;
+const IndexPage = ({ data }: PageProps<Queries.AllPagesQuery>) => {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {data.allMarkdownRemark.edges.map(({ node }: any) => (
+          <li key={node.id}>
+            <Link to={"/posts" + node.frontmatter.slug}>{node.frontmatter.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const query = graphql`
+  query AllPages {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
