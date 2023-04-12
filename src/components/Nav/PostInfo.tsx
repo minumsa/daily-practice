cefe69831fc2575856d4d9028cfc91a9d361b9c4
minumsa <icycyi@naver.com>
2023-04-12 22:23:15 +0900

    Nav 화살표 디자인 변경

--- src/components/Nav/PostInfo.tsx
+++ src/components/Nav/PostInfo.tsx
@@ -4,9 +4,13 @@ import "./PostInfo.css";
 const PostInfo = ({ date }: { date: React.ReactNode }) => {
   return (
     <>
-      <div className="arrow-left">←</div>
+      <div className="arrow-container">
+        <div className="arrow-left"></div>
+      </div>
       <div className="date-created">작성일: {date}</div>
-      <div className="arrow-right">→</div>
+      <div className="arrow-container">
+        <div className="arrow-right"></div>
+      </div>
     </>
   );
 };
