d15c029cb632d1472ea0d77d7faf5a267750e666
minumsa <icycyi@naver.com>
2023-04-13 19:03:22 +0900

    다크모드 작업 일부 성공

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import "./Layout.css";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "../theme";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}  
+  body {        
+    background-color: ${props => props.theme.bgColor};
+    color:${props => props.theme.textColor}
+  }
+
+  .line {
+    background-color: ${props => props.theme.lineColor};
+  }
+`;
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
-  return <div className="layout-container">{children}</div>;
+  const isDarkMode = false;
+
+  return (
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <div className="layout-container">{children}</div>
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
---
+++ src/theme.tsx
@@ -0,0 +1,18 @@
+export const lightTheme = {
+  bgColor: "#f4f4f4",
+  textColor: "#000000",
+  lineColor: "#ff0b0b",
+};
+
+export const darkTheme = {
+  bgColor: "#282c35",
+  textColor: "#fff",
+  lineColor: "#0b3aff",
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;
