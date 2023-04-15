0ff29f213a99dc0d91fe16ac65762594458e81c2
minumsa <icycyi@naver.com>
2023-04-16 00:18:44 +0900

    GlobalStyle에 폰트 명령어 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,9 +15,14 @@ export interface ThemeType {
 
 const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
   ${reset}  
+  
+  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css");
+
   body {        
     background-color: ${props => props.theme.bgColor};
-    color:${props => props.theme.Color}
+    color:${props => props.theme.Color};
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo",
+    "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 
   .line {
