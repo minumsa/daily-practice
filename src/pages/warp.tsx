bd565f1241550f25f13cd172a6dd271f1c6ec3d9
유일 <83910706+minumsa@users.noreply.github.com>
2023-04-20 17:26:21 +0900

    직전 랜덤 페이지로 이동하지 않기

--- src/pages/warp.tsx
+++ src/pages/warp.tsx
@@ -4,32 +4,48 @@ import Warp from "../components/HomePage/Main/Warp";
 import Layout from "../components/Layout";
 import { PageProps, graphql } from "gatsby";
 
+// 페이지 총 수를 받아서 무작위 페이지 번호를 반환하는 함수
+const getRandomPage = (totalCount: number) => {
+  return Math.floor(Math.random() * (totalCount - 1)) + 1;
+};
+
 const WarpPage = ({ data }: PageProps<Queries.AllPagesQuery>) => {
-  const [timer, setTimer] = useState<number>(0);
-
   useEffect(() => {
+    // 페이지 수 가져오기
     const totalPageCount = data.allMarkdownRemark.totalCount;
-    const randomPage = Math.floor(Math.random() * (totalPageCount - 1)) + 1;
+    // 마지막 랜덤 페이지 번호 가져오기
+    const lastRandomPage = sessionStorage.getItem("randomPage");
+    // 새로운 랜덤 페이지 번호 만들기
+    let randomPage = getRandomPage(totalPageCount);
 
+    // 마지막 랜덤 페이지 번호와 새로운 랜덤 페이지 번호가 같을 경우 다시 만들기
+    while (lastRandomPage && randomPage === parseInt(lastRandomPage)) {
+      randomPage = getRandomPage(totalPageCount);
+    }
+
+    // 새로운 랜덤 페이지 번호를 sessionStorage에 저장
+    sessionStorage.setItem("randomPage", randomPage.toString());
+
+    // 1초 후 랜덤 페이지로 이동
     const newTimer = window.setTimeout(() => {
       window.location.href = `/posts/${randomPage}`;
     }, 1000);
-    setTimer(newTimer);
 
+    // 언마운트 될 때 타이머 제거
     return () => {
-      clearTimeout(timer);
+      clearTimeout(newTimer);
     };
   }, []);
 
   return (
+    // Layout 컴포넌트를 사용하여 페이지 구성
     <Layout page={""} info={""}>
       <HomePage content={<Warp />} title={""} />
     </Layout>
   );
 };
 
-export default WarpPage;
-
+// GraphQL 쿼리를 사용하여 모든 페이지 정보 가져오기
 export const query = graphql`
   query AllPages {
     allMarkdownRemark {
@@ -49,3 +65,5 @@ export const query = graphql`
     }
   }
 `;
+
+export default WarpPage;
