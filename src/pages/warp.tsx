b81fe97d45fdccd1e0f50a3ffc1d4e8596bea1f4
유일 <83910706+minumsa@users.noreply.github.com>
2023-04-20 16:19:51 +0900

    warp 페이지 기능 만들기

--- src/pages/warp.tsx
+++ src/pages/warp.tsx
@@ -1,9 +1,26 @@
-import React, { FC } from "react";
+import React, { useState, useEffect } from "react";
 import HomePage from "../components/HomePage";
 import Warp from "../components/HomePage/Main/Warp";
 import Layout from "../components/Layout";
+import { PageProps, graphql } from "gatsby";
 
-const WarpPage: FC = () => {
+const WarpPage = ({ data }: PageProps<Queries.AllPagesQuery>) => {
+  const [randomPage, setRandomPage] = useState<number>(0);
+  const [timer, setTimer] = useState<number>(0);
+
+  useEffect(() => {
+    const newRandomPage = Math.floor(Math.random() * data.allMarkdownRemark.totalCount);
+
+    const newTimer = window.setTimeout(() => {
+      window.location.href = `/posts/${newRandomPage}`;
+    }, 1000);
+    setTimer(newTimer);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <Layout page={""} info={""}>
       <HomePage content={<Warp />} title={""} />
@@ -12,3 +29,23 @@ const WarpPage: FC = () => {
 };
 
 export default WarpPage;
+
+export const query = graphql`
+  query AllPages {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            page
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
