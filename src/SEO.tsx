d74d26a04ea6f557756fae98893384a67d27b5ba
minumsa <icycyi@naver.com>
2023-04-26 15:48:42 +0900

    ogSiteName Test

--- src/SEO.tsx
+++ src/SEO.tsx
@@ -4,13 +4,18 @@ import { Helmet } from "react-helmet";
 interface SEOProps {
   title: string;
   ogTitle: string;
+  ogSiteName: string;
 }
 
-const SEO = ({ title, ogTitle }: SEOProps) => {
+const SEO = ({ title, ogTitle, ogSiteName }: SEOProps) => {
   return (
     <Helmet
       title={title}
       meta={[
+        {
+          property: `og:site_name`,
+          content: ogSiteName,
+        },
         {
           property: `og:title`,
           content: ogTitle,
