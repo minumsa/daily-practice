2d05bdb5b9860377a5d7cdfe49830cf5d2f5db85
유일 <83910706+minumsa@users.noreply.github.com>
2023-04-26 11:49:28 +0900

    Connect 컴포넌트 리팩토링

--- src/components/HomePage/Main/Connect.tsx
+++ src/components/HomePage/Main/Connect.tsx
@@ -6,12 +6,15 @@ const Connect: FC = () => {
     <div className="connect-container">
       <div className="connect-title">운영자</div>
       <div className="connect-content">최유일</div>
-      <br />
+      <div className="connect-gap" />
       <div className="connect-title">이메일</div>
       <div>icycyi@naver.com</div>
-      <br />
+      <div className="connect-gap" />
       <div className="connect-title">SNS</div>
-      <div>인스타그램&nbsp;&nbsp;&nbsp;트위터</div>
+      <div>
+        <span className="connect-sns">인스타그램</span>
+        <span>트위터</span>
+      </div>
     </div>
   );
 };
