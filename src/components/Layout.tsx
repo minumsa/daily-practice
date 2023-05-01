f41992e14dfc5e3298c3e8f6e67c68ab73abbb1d
minumsa <icycyi@naver.com>
2023-05-01 13:00:28 +0900

    ThemeProps 만들기

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,10 +14,16 @@ type Props = {
   info?: React.ReactNode;
 };
 
+// void는 JavaScript에서 데이터 타입이 없는 값을 반환할 때 사용되는 예약어다. 이 코드에서 void는 toggleTheme 함수가 반환하는 값이 없음을 나타낸다. toggleTheme 함수는 선택적으로 문자열 타입의 매개변수를 받을 수 있지만, 반환값은 없다. 따라서 void 예약어를 사용하여 명시적으로 반환값이 없음을 나타내고 있다.
+interface ThemeProps {
+  theme: string | null;
+  toggleTheme: (theme?: string) => void;
+}
+
 const Layout: React.FC<Props> = ({ children, page, info }) => {
   return (
     <ThemeToggler>
-      {({ theme, toggleTheme }: { theme: string | null; toggleTheme: (theme?: string) => void }) => {
+      {({ theme, toggleTheme }: ThemeProps) => {
         if (theme == null) {
           return null;
         }
