da187b8f2bea42b43e1f73e27a0456ef2b0d661f
minumsa <icycyi@naver.com>
2023-05-01 13:41:17 +0900

    Top.tsx 각주 달기

--- src/components/MobileUI/Top.tsx
+++ src/components/MobileUI/Top.tsx
@@ -12,12 +12,14 @@ const Top = ({ darkButton, header }: TopProps) => {
   const [visible, setVisible] = useState(false);
 
   const handleHamburgerClick = () => {
+    // visible 상태를 현재 상태의 반대 값으로 업데이트한다.
     setVisible(!visible);
   };
 
   return (
     <header className={header}>
       <div className="top-content">
+        {/* 햄버거 버튼 클릭 시 visible 상태가 true이면 카테고리 컴포넌트가 열리고, false이면 닫힌다. */}
         <div className="top-hamburger-container" onClick={handleHamburgerClick}>
           <div className="top-hamburger">{visible ? <Category /> : null}</div>
         </div>
