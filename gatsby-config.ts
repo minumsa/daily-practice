aeec27a4d7b64e036983ce88eb437f7b8a5c9db0
minumsa <icycyi@naver.com>
2023-06-06 23:54:49 +0900

    siteUrl 변경

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `일상연습`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://blog.divdivdiv.com/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
