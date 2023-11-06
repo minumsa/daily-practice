e3717f0b5eb0efe8e3dc0e1088cc21561a43986c
minumsa <icycyi@naver.com>
2023-11-06 18:33:43 +0900

    warp-container 좌우 가운데 정렬

--- src/components/HomePage/Main/Warp.tsx
+++ src/components/HomePage/Main/Warp.tsx
@@ -4,7 +4,7 @@ import React, { FC } from "react";
 const Warp: FC = () => {
   return (
     <div className="warp-container">
-      <p>과거 혹은 미래로 이동 중입니다...</p>
+      <div>과거 혹은 미래로 이동 중입니다...</div>
     </div>
   );
 };
