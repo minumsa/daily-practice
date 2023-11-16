6f6dd94334c2f7f5629ec82112dcef3be2046dca
유일 <83910706+minumsa@users.noreply.github.com>
2023-11-16 13:02:34 +0900

    copied 알림

--- src/components/Home/Main/Contact.tsx
+++ src/components/Home/Main/Contact.tsx
@@ -1,16 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import "./Contact.css";
 
 const Contact: FC = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    window.navigator.clipboard.writeText("icycyi@naver.com");
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-title">이메일</div>
-      <div>icycyi@naver.com</div>
+      <div style={{ position: "relative", width: "270px" }}>
+        <div>
+          <span
+            className="cotact-email highlighted-text"
+            onClick={() => {
+              handleCopy();
+            }}
+          >
+            icycyi@naver.com
+          </span>
+        </div>
+        {isCopied && <span className="copied-message">Copied!</span>}
+      </div>
       <div className="contact-title">SNS</div>
       <div>
-        <a href="https://www.instagram.com/yoon.o" target="_blank">
-          <div>Instagram</div>
-        </a>
+        <span className="highlighted-text">
+          <a href="https://www.instagram.com/yoon.o" target="_blank">
+            Instagram
+          </a>
+        </span>
       </div>
     </div>
   );
