891b37f725803248d1f0627b2842b64f2fa3cf92
유일 <83910706+minumsa@users.noreply.github.com>
2024-04-19 11:00:44 +0900

    일러두기 목록 1개 제거

--- src/components/Home/Main/Explanation.tsx
+++ src/components/Home/Main/Explanation.tsx
@@ -14,7 +14,6 @@ const Explanation = () => {
             홈페이지 타이틀 <span className={styles.highlight}>일상연습</span>을 클릭하면 메인
             화면으로 이동합니다.
           </li>
-          <li>만약 돌아가기 아이콘이 발견되지 않는다면 아직 남아 있는 글이 있는 것입니다.</li>
           <li>글 하단에는 해당 작성일이 표시되어 있습니다.</li>
           <li>
             작성일 양옆의 화살표
